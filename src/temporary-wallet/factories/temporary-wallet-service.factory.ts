1a71679c550ee3d771c9326b12ddef6af30cd440
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-17 17:16:08 +0600

    implements factory

--- src/temporary-wallet/factories/temporary-wallet-service.factory.ts
+++ src/temporary-wallet/factories/temporary-wallet-service.factory.ts
@@ -1,16 +1,31 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { TemporaryWalletService } from '../temporary-wallet.service';
 import { NetworkEnum } from '../../common/network.enum';
 import { CoinEnum } from '../../common/coin.enum';
+import { ModuleRef } from '@nestjs/core';
+import { BitcoinTemporaryWalletService } from '../services/bitcoin-temporary-wallet.service';
 
 @Injectable()
 export class TemporaryWalletServiceFactory {
-  constructor() {}
+  private static readonly supportedMap = {
+    [NetworkEnum.BTC]: {
+      [CoinEnum.BTC]: BitcoinTemporaryWalletService,
+    },
+  };
+
+  constructor(private readonly moduleRef: ModuleRef) {}
 
   public for(
     network: NetworkEnum,
-    coin: CoinEnum = network,
+    coin: CoinEnum = network as unknown as CoinEnum,
   ): TemporaryWalletService {
-    throw new NotImplementedException();
+    const temporaryWalletService =
+      TemporaryWalletServiceFactory.supportedMap[network]?.[coin];
+
+    if (!temporaryWalletService) {
+      throw new Error(`Unsupported ${network}:${coin}`);
+    }
+
+    return this.moduleRef.get(temporaryWalletService);
   }
 }

ae6953307b3feb34379383db3e1989983a1b64c8
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-17 18:21:38 +0600

    bridge controller impl

---
+++ src/temporary-wallet/controller/bridge.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { TemporaryWalletServiceFactory } from '../factories/temporary-wallet-service.factory';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { Balance } from '../temporary-wallet.service';
+import { NetworkEnum } from '../../common/network.enum';
+import { CoinEnum } from '../../common/coin.enum';
+
+@Controller()
+@ApiTags('Bridge')
+export class BridgeController {
+  constructor(
+    private readonly temporaryWalletServiceFactory: TemporaryWalletServiceFactory,
+  ) {}
+
+  @Post(':network/:coin/wallet')
+  @ApiParam({
+    name: 'network',
+    enum: NetworkEnum,
+  })
+  @ApiParam({
+    name: 'coin',
+    enum: CoinEnum,
+  })
+  public async createWallet(
+    @Param('network') network: NetworkEnum,
+    @Param('coin') coin: CoinEnum,
+  ) {
+    return this.temporaryWalletServiceFactory.for(network, coin).createWallet();
+  }
+
+  @Get(':network/:coin/wallet/:address/balance')
+  @ApiParam({
+    name: 'network',
+    enum: NetworkEnum,
+  })
+  @ApiParam({
+    name: 'coin',
+    enum: CoinEnum,
+  })
+  public async getBalance(
+    @Param('network') network: NetworkEnum,
+    @Param('coin') coin: CoinEnum,
+    @Param('address') address: string,
+  ): Promise<Balance> {
+    return this.temporaryWalletServiceFactory
+      .for(network, coin)
+      .getBalance(address);
+  }
+}
--- src/temporary-wallet/temporary-wallet.module.ts
+++ src/temporary-wallet/temporary-wallet.module.ts
@@ -3,14 +3,14 @@ import { TemporaryWalletServiceFactory } from './factories/temporary-wallet-serv
 import { BitcoinModule } from '../bitcoin/bitcoin.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TemporaryWallet } from './dao/entity/temporary-wallet';
-import { TemporaryWalletController } from './controller/temporary-wallet.controller';
+import { BridgeController } from './controller/bridge.controller';
 import { BitcoinTemporaryWalletService } from './services/bitcoin-temporary-wallet.service';
 import { CreateTemporaryWalletService } from './services/create-temporary-wallet.service';
 import { TemporaryWalletPgRepository } from './repositories/temporary-wallet-pg.repository';
 
 @Module({
   imports: [BitcoinModule, TypeOrmModule.forFeature([TemporaryWallet])],
-  controllers: [TemporaryWalletController],
+  controllers: [BridgeController],
   providers: [
     TemporaryWalletServiceFactory,
     BitcoinTemporaryWalletService,

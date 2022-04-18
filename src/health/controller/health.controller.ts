f439df38cae897a318fa7f2f47e497adfbc30ead
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-19 00:02:07 +0600

    eth health check impl

--- src/health/controller/health.controller.ts
+++ src/health/controller/health.controller.ts
@@ -2,11 +2,17 @@ import { Controller, Get } from '@nestjs/common';
 import { HealthServiceResponse } from './health-service.response';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { BitcoinRpcClient } from '../../bitcoin/rpc/bitcoin-rpc.client';
+import { EthereumWeb3Service } from '../../ethereum/services/ethereum-web3.service';
+import { LoggerService } from '@ledius/logger';
 
 @Controller()
 @ApiTags('Health')
 export class HealthController {
-  constructor(private readonly bitcoinRpcClient: BitcoinRpcClient) {}
+  constructor(
+    private readonly bitcoinRpcClient: BitcoinRpcClient,
+    private readonly ethereumWeb3Service: EthereumWeb3Service,
+    private readonly logger: LoggerService,
+  ) {}
 
   @Get('health')
   @ApiOkResponse({
@@ -21,6 +27,16 @@ export class HealthController {
           .uptime()
           .then(() => true)
           .catch(() => false),
+      },
+      {
+        serviceName: 'Ethereum Network',
+        available: await this.ethereumWeb3Service.eth
+          .getBlockNumber()
+          .then(() => true)
+          .catch((err) => {
+            this.logger.log({ err });
+            return false;
+          }),
       },
     ];
   }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './controller/health.controller';
 import { BitcoinModule } from '../bitcoin/bitcoin.module';
+import { EthereumModule } from '../ethereum/ethereum.module';
+import { LoggerModule } from '@ledius/logger';
 
 @Module({
-  imports: [BitcoinModule],
+  imports: [BitcoinModule, EthereumModule, LoggerModule],
   controllers: [HealthController],
 })
 export class HealthModule {}

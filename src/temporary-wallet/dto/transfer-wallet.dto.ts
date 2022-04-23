3ef68bcba219c73244597dbe10044acad8d5362f
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-23 15:12:34 +0600

    add validation

--- src/temporary-wallet/dto/transfer-wallet.dto.ts
+++ src/temporary-wallet/dto/transfer-wallet.dto.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
+import { IsDefined } from 'class-validator';
 
 export class TransferWalletDto {
   @ApiProperty()
+  @IsDefined()
   public from: string;
 
   @ApiProperty()
+  @IsDefined()
   public to: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: 'string',
+  })
+  @IsDefined()
   @Transform((v) => BigInt(v.value))
   public amount: bigint;
 }

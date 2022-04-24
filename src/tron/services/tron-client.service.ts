1f6ff82a0b8bae61b37c971abdc610fd3cb3d3d1
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-24 13:44:06 +0600

    account balance method impl

--- src/tron/services/tron-client.service.ts
+++ src/tron/services/tron-client.service.ts
@@ -13,10 +13,46 @@ export class TronClientService {
 
     return data;
   }
+
+  public async getAccountBalance(
+    params: GetAccountBalanceParams,
+  ): Promise<GetAccountBalanceResult> {
+    const { data } = await lastValueFrom(
+      this.http.post('/wallet/getaccountbalance', {
+        account_identifier: params.accountIdentifier,
+        block_identifier: params.blockIdentifier,
+        visible: params.visible,
+      }),
+    );
+
+    return {
+      blockIdentifier: data.block_identifier,
+      balance: data.balance,
+    };
+  }
 }
 
 export interface GenerateAddressResult {
   privateKey: string;
   address: string;
   hexAddress: string;
+}
+
+export interface GetAccountBalanceParams {
+  accountIdentifier: {
+    address: string;
+  };
+  blockIdentifier: {
+    hash: string;
+    number: number;
+  };
+  visible: boolean;
+}
+
+export interface GetAccountBalanceResult {
+  balance: number;
+  blockIdentifier: {
+    hash: string;
+    number: number;
+  };
 }

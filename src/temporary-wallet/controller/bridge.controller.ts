d2bcea62e7c29549af340dcd6ac38dcb97fe5395
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-24 19:21:21 +0600

    bridge guards

--- src/temporary-wallet/controller/bridge.controller.ts
+++ src/temporary-wallet/controller/bridge.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TemporaryWalletServiceFactory } from '../factories/temporary-wallet-service.factory';
-import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBasicAuth,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Balance } from '../temporary-wallet.service';
 import { NetworkEnum } from '../../common/network.enum';
 import { CoinEnum } from '../../common/coin.enum';
 import { TransferWalletDto } from '../dto/transfer-wallet.dto';
 import { WalletResponse } from './wallet.response';
+import { AppGuard } from '../../application/guard/app.guard';
 
 @Controller()
 @ApiTags('Bridge')
@@ -23,6 +29,8 @@ export class BridgeController {
     name: 'coin',
     enum: CoinEnum,
   })
+  @UseGuards(AppGuard)
+  @ApiBasicAuth()
   public async createWallet(
     @Param('network') network: NetworkEnum,
     @Param('coin') coin: CoinEnum,
@@ -43,6 +51,8 @@ export class BridgeController {
     name: 'coin',
     enum: CoinEnum,
   })
+  @UseGuards(AppGuard)
+  @ApiBasicAuth()
   public async getBalance(
     @Param('network') network: NetworkEnum,
     @Param('coin') coin: CoinEnum,
@@ -62,6 +72,8 @@ export class BridgeController {
     name: 'coin',
     enum: CoinEnum,
   })
+  @UseGuards(AppGuard)
+  @ApiBasicAuth()
   public async transfer(
     @Param('network') network: NetworkEnum,
     @Param('coin') coin: CoinEnum,
@@ -81,6 +93,8 @@ export class BridgeController {
     name: 'coin',
     enum: CoinEnum,
   })
+  @UseGuards(AppGuard)
+  @ApiBasicAuth()
   public async estimateFee(
     @Param('network') network: NetworkEnum,
     @Param('coin') coin: CoinEnum,
@@ -95,6 +109,8 @@ export class BridgeController {
   @ApiOkResponse({
     description: 'Key of object is network, value is symbol supported coins',
   })
+  @UseGuards(AppGuard)
+  @ApiBasicAuth()
   public async supportedCoins(): Promise<Record<string, string[]>> {
     const map = this.temporaryWalletServiceFactory.supportedMap;
 

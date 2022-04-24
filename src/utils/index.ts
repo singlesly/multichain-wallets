599701cc1f06bba218097eb4c924f49154dae2e9
Artem Ilinykh <devsinglesly@gmail.com>
2022-04-24 11:24:00 +0600

    base58check tools

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
-import base58check from 'bs58check';
+import bs58check from 'bs58check';
 
 export function hexToBase58Check(hex: string): string {
-  return base58check.encode(hex);
+  return bs58check.encode(Buffer.from(hex, 'hex'));
+}
+
+export function base58CheckToHex(base58check: string): string {
+  return bs58check.decode(base58check).toString('hex').toUpperCase();
 }

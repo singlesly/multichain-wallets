08845c1547e438ce1d74003dc71fe05a4ab8d29f
Artem Ilinykh <devsinglesly@gmail.com>
2022-05-02 19:49:07 +0600

    handle exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { BitcoinModule } from '@app/bitcoin/bitcoin.module';
 import { DatabaseModule } from '@app/database/database.module';
 import { EncryptModule } from '@app/encrypt/encrypt.module';
 import { WalletModule } from '@app/wallet/wallet.module';
-import { APP_INTERCEPTOR, RouterModule } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, RouterModule } from '@nestjs/core';
 import { RequestContextModule } from '@ledius/request-context';
 import { LoggerModule } from '@ledius/logger';
 import { HealthModule } from '@app/health/health.module';
@@ -12,6 +12,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import path from 'path';
 import { ApplicationModule } from '@app/application/application.module';
 import { BridgeModule } from '@app/bridge/bridge.module';
+import { BaseExceptionFilter } from '@app/common/filters/base-exception.filter';
+import { UnhandledExceptionFilter } from '@app/common/filters/unhandled-exception.filter';
 
 @Module({
   imports: [
@@ -35,6 +37,14 @@ import { BridgeModule } from '@app/bridge/bridge.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: BaseExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: UnhandledExceptionFilter,
     },
   ],
 })

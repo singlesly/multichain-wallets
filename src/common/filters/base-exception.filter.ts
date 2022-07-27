0516f53015ba3829c2a2355002e2b0fa7375df34
Artem Ilinykh <devsinglesly@gmail.com>
2022-07-27 14:52:38 +0600

    logging exception

--- src/common/filters/base-exception.filter.ts
+++ src/common/filters/base-exception.filter.ts
@@ -1,14 +1,11 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { BaseException } from '@app/common/base-exception';
 import { Response } from 'express';
-import {LoggerService} from "@ledius/logger";
+import { LoggerService } from '@ledius/logger';
 
 @Catch(BaseException)
 export class BaseExceptionFilter implements ExceptionFilter {
-  constructor(
-      private readonly logger: LoggerService
-  ) {
-  }
+  constructor(private readonly logger: LoggerService) {}
 
   public catch(exception: BaseException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();

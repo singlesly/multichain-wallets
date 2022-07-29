922eac04edd3e575b7e42936bfe3130a004068c1
Artem Ilinykh <devsinglesly@gmail.com>
2022-07-30 04:07:32 +0600

    add bearer auth

--- src/swagger.ts
+++ src/swagger.ts
@@ -7,6 +7,7 @@ export async function useSwagger(app: INestApplication): Promise<void> {
   const config = new DocumentBuilder()
     .setTitle('Crypto Bridge')
     .addBasicAuth()
+    .addBearerAuth()
     .setDescription('The cryptocurrency bridge API description')
     .setVersion('1.0')
     .build();

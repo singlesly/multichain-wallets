767eb4522a34fd0526420c916abad30665d08b40
Artem Ilinykh <devsinglesly@gmail.com>
2022-07-30 03:01:12 +0600

    use guard

--- src/application/guard/app.guard.ts
+++ src/application/guard/app.guard.ts
@@ -2,11 +2,30 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { Application } from '../dao/entity/application';
 
+type TokenType = 'bearer' | 'basic';
+
 @Injectable()
 export class AppGuard implements CanActivate {
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
-    const encodedAuth = request.headers.authorization?.split(' ')[1];
+    const authorization = request.headers.authorization;
+    if (!authorization) {
+      return false;
+    }
+
+    const tokenType: TokenType = authorization.split(' ')[0] as TokenType;
+    const encodedAuth = authorization.split(' ')[1] as `${string}:${string}`;
+
+    if (tokenType === 'basic') {
+      return await this.handleBasic(encodedAuth);
+    }
+
+    return false;
+  }
+
+  private async handleBasic(
+    encodedAuth: `${string}:${string}`,
+  ): Promise<boolean> {
     if (!encodedAuth) {
       return false;
     }

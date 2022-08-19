7ad2c91b150ca772c20487ff09cc7a385f8c77b5
Artem Ilinykh <devsinglesly@gmail.com>
2022-08-20 04:20:16 +0600

    feat: implement virtual transaction service

---
+++ src/transaction/repositories/virtual-transaction-pg.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { VirtualTransaction } from '@app/transaction/dao/entity/virtual-transaction';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BaseException } from '@app/common/base-exception';
+import { WebErrorsEnum } from '@app/common/web-errors.enum';
+
+@Injectable()
+export class VirtualTransactionPgRepository {
+  constructor(
+    @InjectRepository(VirtualTransaction)
+    private readonly repository: Repository<VirtualTransaction>,
+  ) {}
+
+  public async getByIdOrFail(id: string): Promise<VirtualTransaction> {
+    const found = await this.repository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!found) {
+      throw new BaseException({
+        statusCode: WebErrorsEnum.NOT_FOUND,
+        message: 'Transaction not found',
+      });
+    }
+
+    return found;
+  }
+
+  public async save(
+    virtualTransaction: VirtualTransaction,
+  ): Promise<VirtualTransaction> {
+    return await this.repository.save(virtualTransaction);
+  }
+}

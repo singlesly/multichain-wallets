902f38b392629da0b138c3ba19fd41b1e71c62f1
Artem Ilinykh <devsinglesly@gmail.com>
2023-01-19 00:51:37 +0600

    add tokens

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -3,9 +3,11 @@ import { NetworkModule } from '@app/network/network.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Token } from '@app/token/dao/entity/token';
 import { TokenService } from '@app/token/services/token.service';
+import { TokenController } from '@app/token/controllers/token.controller';
 
 @Module({
   imports: [NetworkModule, TypeOrmModule.forFeature([Token])],
+  controllers: [TokenController],
   providers: [TokenService],
 })
 export class TokenModule {}

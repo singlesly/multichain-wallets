84e7f02ce9ff8ed21e1ada9513c38d988c0701c1
Artem Ilinykh <devsinglesly@gmail.com>
2023-03-10 05:14:03 +0600

    add fiat convert

--- src/payment/services/get-payment-link.service.ts
+++ src/payment/services/get-payment-link.service.ts
@@ -4,22 +4,36 @@ import {
   GetPaymentLinkResult,
   TinkoffService,
 } from '@app/tinkoff/services/tinkoff.service';
+import { CryptoCurrencyConverterService } from '@app/converter/services/coin-gecko/crypto-currency-converter.service';
 
 @Injectable()
 export class GetPaymentLinkService {
   constructor(
     private readonly getPaymentService: GetPaymentService,
     private readonly tinkoffService: TinkoffService,
+    private readonly cryptoCurrencyConverterService: CryptoCurrencyConverterService,
   ) {}
 
   public async getPaymentLink(
     paymentId: string,
   ): Promise<GetPaymentLinkResult> {
     const payment = await this.getPaymentService.getPaymentById(paymentId);
+    const fiatAmount = payment.getFiatAmount();
+    const amount = await this.cryptoCurrencyConverterService.convert(
+      {
+        amount: BigInt(fiatAmount.amountScaled),
+        symbol: fiatAmount.tokenSymbol,
+        decimals: fiatAmount.decimals,
+      },
+      {
+        symbol: 'rub',
+        decimals: 2,
+      },
+    );
 
     return this.tinkoffService.getPaymentLink({
       orderId: payment.orderId,
-      amount: Number(payment.getFiatAmount().amountScaled),
+      amount: Number(amount),
     });
   }
 }
